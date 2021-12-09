78875d83b75b2e1f7f92f08c42fedc3748b7b897
sqoger <48029277+sqoger@users.noreply.github.com>
2021-12-09 23:26:21 +0100

    todo quarters

--- EisenhowerMain.cs
+++ EisenhowerMain.cs
@@ -1,17 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace EisenhowerCore
 {
     public class EisenhowerMain
-    {
+    {        
         static void Main(string[] args)
         {
+
+            //TodoItem todo1 = new TodoItem("tdgewf1", V);
+            //TodoItem todo2 = new TodoItem("td2avsdvcdsa", V);
+            //todo1.Mark();
+            //Console.WriteLine(todo1.TdToString());
+            //Console.WriteLine(todo2.TdToString())
             DateTime V = DateTime.Now;
-            TodoItem todo1 = new TodoItem("tdgewf1", V);
-            TodoItem todo2 = new TodoItem("td2avsdvcdsa", V);
-            todo1.Mark();
-            Console.WriteLine(todo1.TdToString());
-            Console.WriteLine(todo2.TdToString());
+            List<TodoItem> list = new List<TodoItem>()
+                {
+                    new TodoItem("tdgewf1", V),
+                    new TodoItem("td2avsdvcdsa", V),
+                    new TodoItem("asdaf", V)
+                };
+            foreach (var item in list)
+            {
+                Console.WriteLine(item.TdToString());
+                item.Mark();
+            }
+
         }
+
     }
 }
--- TodoQuarter.cs
+++ TodoQuarter.cs
@@ -6,6 +6,57 @@ namespace EisenhowerCore {
 
     public class TodoQuarter {
 
+        public List<TodoItem> TodoItems { get; set; } = new List<TodoItem>();
+
+        List<TodoItem> TodoQuart()
+        {
+            return TodoItems;
+        }
+
+        public List<TodoItem> AddItem(string title, DateTime deadline)
+        {
+            TodoItems.Add(new TodoItem(title,deadline) { Title = title, Deadline = deadline});
+            return TodoItems;
+        }
+
+        public List<TodoItem> RemoveItem(int index)
+        {
+            TodoItems.RemoveAt(index);
+            return TodoItems;
+        }
+
+        public List<TodoItem> ArchiveItems()
+        {
+            for (int i = 0; i < TodoItems.Count; i++)
+            {
+                if (TodoItems[i].IsDone == true)
+                {
+                    TodoItems.RemoveAt(i);
+                }
+            }
+            return TodoItems;
+        }
+
+        public TodoItem GetItem(int index)
+        {
+            return TodoItems[index];
+        }
+
+        private List<TodoItem> GetItems()
+        {
+            return TodoItems;
+        }
+
+        public List<string> QuartToString()
+        {
+            List<string> QuartStringList = new List<string>();
+
+            for (int i = 0; i < TodoItems.Count; i++)
+            {
+                QuartStringList.Add($"{i + 1}. {TodoItems[i]}");
+            }
+            return QuartStringList;
+        }
     }
 
 }
